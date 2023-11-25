8139ded4d8f20dd7246d23a4f8a8d2452baa7e62
Kazi Khaled Nur <kazikhalednur@gmail.com>
2023-11-25 13:12:01 +0600

    update some configuration

--- root/urls.py
+++ root/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("accounts/", include("accounts.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("events/", include("events.urls")),
